339c31e36345126a58288d902206ba7664bea637
JDM <test@xyz.com>
2024-11-15 22:39:09 +0000

    added loop for multi ping

--- app/main.py
+++ app/main.py
@@ -7,8 +7,13 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client_socket, _ = server_socket.accept() # wait for client
-    client_socket.send(b"+PONG\r\n")
 
+    while True:
+        request: bytes = client_socket.recv(512)
+        data: str = request.decode()
+
+        if "ping" in data.lower():
+            client_socket.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
