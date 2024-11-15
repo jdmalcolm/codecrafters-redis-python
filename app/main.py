36a7c15b5dd35d3af9615a833a1da6dbb8f07144
JDM <test@xyz.com>
2024-11-15 23:47:51 +0000

    added loop for multiple requests

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ def connect(connection: socket.socket) -> None:
         connected: bool = True
         while connected:
             request: bytes = connection.recv(512)
-            print(f"Received: {request.decode()}")
 
             connected = bool(request)
             if "ping" in request.decode().lower():
@@ -15,16 +14,20 @@ def connect(connection: socket.socket) -> None:
                 connection.send(pong.encode())
 
 
-
 def main() -> None:
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket, client_addr = server_socket.accept() # wait for client
-    print(f"Accepted connection [{client_addr[0]}:{str(client_addr[1])}]")
 
-    connect(client_socket)
+    while True:
+        client_socket, client_addr = server_socket.accept()  # wait for client
+        print(f"Accepted connection [{client_addr[0]}:{str(client_addr[1])}]")
+
+        # connect(client_socket)
+        thread: threading.Thread = threading.Thread(target=connect, args=[client_socket])
+        thread.start()
+
 
 if __name__ == "__main__":
     main()
