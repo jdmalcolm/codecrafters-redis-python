44905e3b11359edf396d1e4bc94598f84f3b561b
JDM <test@xyz.com>
2024-11-15 22:19:20 +0000

    fixed tuple issue

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket = server_socket.accept() # wait for client
-    client_socket.sendall(b"+PONG\r\n")
+    client_socket, _ = server_socket.accept() # wait for client
+    client_socket.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
