133b7c8b5a937657a228ee00d33d9a3f2cde12c3
JDM <test@xyz.com>
2024-11-15 23:36:32 +0000

    refactored connect

--- app/main.py
+++ app/main.py
@@ -1,19 +1,30 @@
 import socket  # noqa: F401
+import threading
 
 
-def main():
+def connect(connection: socket.socket) -> None:
+    with connection:
+        connected: bool = True
+        while connected:
+            request: bytes = connection.recv(512)
+            print(f"Received: {request.decode()}")
+
+            connected = bool(request)
+            if "ping" in request.decode().lower():
+                pong: str = "+PONG\r\n"
+                connection.send(pong.encode())
+
+
+
+def main() -> None:
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket, _ = server_socket.accept() # wait for client
+    client_socket, client_addr = server_socket.accept() # wait for client
+    print(f"Accepted connection [{client_addr[0]}:{str(client_addr[1])}]")
 
-    while True:
-        request: bytes = client_socket.recv(512)
-        data: str = request.decode()
-
-        if "ping" in data.lower():
-            client_socket.send(b"+PONG\r\n")
+    connect(client_socket)
 
 if __name__ == "__main__":
     main()
